78d7546fc0e7f043d809719acd49f33fc0dca956
PasunuriSaichandu <saichandupasunuri2002@gmail.com>
2026-02-25 10:42:24 +0530

    Add files via upload

---
+++ webpush-service-worker.js
@@ -0,0 +1,3 @@
+importScripts('https://api.pushio.com/webpush/sdk/service_min.js');
+var config = '{"appserviceKey":"BO3bPAI2lycIq7PQXjgj9l5mimNen_4dw2QrYab9fmyCEfHCrbHcDKsoKbt4HGNi0-6iaK-0tjkwspC7QrA4iFQ=","apiKey":"ABEX404zBdi5WLSFAKdSXvFWg","accountToken":"ABElKaqpK-3l_zDeOp-XArlkg","appver":"0.0.0","apiHost":"https://abr16c0-webpush.oraclersys.com","lazy":false}';
+orawp.init(config);
